323ccf184d5e4e737eea2d1121e8df72a3032443
nguyentungnm-spec <nguyentungnm@gmail.com>
2026-05-02 21:45:54 +0700

    Fix checkstyle naming and formatting issues

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         // LỖI: Tên biến có dấu gạch dưới (vi phạm LocalVariableName)
-        int my_variable = 10;
-
-        System.out.println(my_variable);
+        int soThuNhat = 10;
+        System.out.println(soThuNhat);
     }
 }
