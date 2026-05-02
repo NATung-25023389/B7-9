d7683e44c30b37c42070ed0f1c9f4db30ea64d25
nguyentungnm-spec <nguyentungnm@gmail.com>
2026-05-02 21:20:05 +0700

    Add intentional checkstyle error for testing

---
+++ src/main/java/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        // LỖI: Tên biến có dấu gạch dưới (vi phạm LocalVariableName)
+        int so_thu_nhat = 10;
+
+        System.out.println(so_thu_nhat);
+    }
+}
